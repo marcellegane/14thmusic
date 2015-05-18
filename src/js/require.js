794118157ccbcb65930931313222122bce0dbb8b
Marcel Legane <mlegane@gmail.com>
2015-05-18 20:59:10 +0100

    Add min-height media-queries

--- src/js/require.js
+++ src/js/require.js
@@ -1,50 +1,85 @@
-var site = (function($) {
 
-    var $win = $(window);
+var samples = [];
 
-    var width = function() {
-        return $win.width();
-    };
-
-    var height = function() {
-        return $win.height();
-    };
-
-    var debounce = function(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this,
-                args = arguments;
-
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
+$('.sample').each(function(i) {
+    var audio = new Audio,
+        src = $(this).data('file'),
+        sample = {
+            audio: audio,
+            src: src
         };
-    };
 
-    var debouncedResize = function() {
-        width();
-        height();
-    };
+    samples.push(sample);
+});
 
-    var resizeEvents = debounce(debouncedResize,250);
+$.each(samples, function(index, sample) {
+    sample.audio.addEventListener('timeupdate', function() {
+        if (sample.audio.currentTime >= sample.audio.duration - 0.2) {
+            sample.audio.currentTime = 0;
+            sample.audio.play(0);
+        }
+    });
+});
 
-    if (window.addEventListener) {
-        window.addEventListener('resize',resizeEvents);
-    } else {
-        window.attachEvent('resize',resizeEvents);
+function setAudioSrc(sample,url) {
+    sample.src = url;
+}
+
+function setAudioFormat (ext) {
+    $.each(samples, function(index, sample) {
+        setAudioSrc(sample.audio,sample.src + ext);
+    });
+}
+
+function canPlayAudio(ext) {
+    var a = document.createElement('audio');
+    return ( !! (a.canPlayType && a.canPlayType('audio/' + ext + ';').replace(/no/, '')));
+}
+
+function getAudioExtension() {
+    var extension = 'wav';
+
+    if (canPlayAudio('ogg')) {
+        extension = '.ogg';
+    }
+    else if (canPlayAudio('mp4')) {
+        extension = '.m4a';
+    }
+    else if (canPlayAudio('aac')) {
+        extension = '.aac';
+    }
+    else if (canPlayAudio('mp3')) {
+        extension = '.mp3';
     }
 
-    return {
-        $win: $win,
-        width: width,
-        height: height
-    };
+    return extension;
+}
 
-})(jQuery);
+setAudioFormat(getAudioExtension());
+
+$.each(samples, function(index, sample) {
+    sample.audio.volume = 0;
+    sample.audio.play();
+});
+
+// Interaction
+
+$('.sample').on('click', function() {
+    var index = $(this).data('index');
+
+    if ($(this).hasClass('playing')) {
+        $(this).removeClass('playing');
+
+        $(samples[index].audio).animate({
+            volume: 0
+        }, 1000);
+    } else {
+        $(this).addClass('playing');
+
+        $(samples[index].audio).animate({
+            volume: 1
+        }, 1000);
+    }
+
+    return false;
+});
