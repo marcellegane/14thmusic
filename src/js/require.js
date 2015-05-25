290c1c563fa1f6c815f0a384265697095437563c
Marcel Legane <mlegane@gmail.com>
2015-05-25 22:31:51 +0100

    New game prototype

--- src/js/require.js
+++ src/js/require.js
@@ -1,104 +1,53 @@
 //------------------------------------------------------------------------
-//  $Samples
-//------------------------------------------------------------------------
-
-
-var samples = [],
-    audioLoad = 0,
-    extension = '.wav';
-
-if (canPlayAudio('ogg')) {
-    extension = '.ogg';
-}
-else if (canPlayAudio('mp4')) {
-    extension = '.m4a';
-}
-else if (canPlayAudio('aac')) {
-    extension = '.aac';
-}
-else if (canPlayAudio('mp3')) {
-    extension = '.mp3';
-}
-
-function canPlayAudio(ext) {
-    var a = document.createElement('audio');
-    return ( !! (a.canPlayType && a.canPlayType('audio/' + ext + ';').replace(/no/, '')));
-}
-
-$('.sample').each(function(i) {
-    var audio = new Audio(),
-        src = $(this).data('file');
-
-    audio.src = src + extension;
-    audio.onloadeddata = function() {
-        console.log('load' + i);
-        audioLoad++;
-        if (audioLoad === 14) {
-            console.log('audioLoad');
-            runSamples();
-        }
-    };
-
-    samples.push(audio);
-});
-
-
-//------------------------------------------------------------------------
-//  $Looping event listener
-//------------------------------------------------------------------------
-
-
-var preloop;
-
-if (extension === '.m4a') {
-    preloop = 0.46;
-} else {
-    preloop = 0.2;
-}
-
-samples[0].addEventListener('timeupdate', function() {
-    if (this.currentTime >= this.duration - preloop) {
-        $.each(samples, function(index, sample){
-            sample.currentTime = 0;
-            sample.play(0);
-        });
-    }
-});
-
-
-//------------------------------------------------------------------------
 //  $Load functions
 //------------------------------------------------------------------------
 
 
-function runSamples() {
-
+$(window).load(function() {
     $('body').addClass('loaded');
+});
 
-    $.each(samples, function(index, sample) {
-        this.currentTime = 0;
-        this.play();
-        this.volume = 0;
-    });
 
-    $('.sample').on('click', function() {
-        var index = $(this).data('index');
+//------------------------------------------------------------------------
+//  $Game
+//------------------------------------------------------------------------
 
-        if ($(this).hasClass('playing')) {
-            $(this).removeClass('playing');
 
-            $(samples[index]).animate({
-                volume: 0
-            }, 1000);
+var selected = 0,
+    selectMax = 4,
+    found = 0,
+    $sample = $('.sample'),
+    $found = $('.found'),
+    play = true;
+
+var sampleSelect = function() {
+    if (play) {
+        if ($(this).hasClass('is-chosen')) {
+            $(this).removeClass('is-chosen');
+            selected--;
+
+            if ($(this).data('code') === true) {
+                found--;
+                $found.html(found);
+            }
         } else {
-            $(this).addClass('playing');
+            if (selected === 4) {
+                return;
+            }
 
-            $(samples[index]).animate({
-                volume: 1
-            }, 1000);
+            $(this).addClass('is-chosen');
+            selected++;
+
+            if ($(this).data('code') === true) {
+                found++;
+                $found.html(found);
+                if (found === 4) {
+                    play = false;
+                    $('body').addClass('found');
+                }
+            }
         }
+    }
+};
 
-        return false;
-    });
-
-}
+$sample.on('click', sampleSelect);
